b3a20b8e0eabcb8a131bb9a38c3e656028ef3893
Владислав <koldaev18@mail.ru>
2023-02-07 20:39:22 +0300

    Неверный API ключ

--- WeatherApp2/weather/views.py
+++ WeatherApp2/weather/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+import requests
+
 def index(request):
+    appid='4d1a908aa1d527f34f695cdff47b27fc'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid="+appid
+
+    city='London'
+    res = requests.get(url.format(city))
+
+    print(res.text)
+
     return render(request, 'weather/index.html')
-# Create your views here.
+
+
+
