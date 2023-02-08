f649f190bd8436a196f417f18743abf548895fb7
Владислав <koldaev18@mail.ru>
2023-02-08 12:31:38 +0300

    Готовый проект погоды

--- WeatherApp2/weather/models.py
+++ WeatherApp2/weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class City(models.Model):
+    name = models.CharField(max_length=40)
+
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
--- WeatherApp2/weather/views.py
+++ WeatherApp2/weather/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 def index(request):
     appid='4d1a908aa1d527f34f695cdff47b27fc'
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid="+appid
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid="+appid
 
-    city='London'
-    res = requests.get(url.format(city))
+    if(request.method=='POST'):
+        form=CityForm(request.POST)
+        form.save()
 
-    print(res.text)
+    form=CityForm
+    cities=City.objects.all()
 
-    return render(request, 'weather/index.html')
+    all_cities=[]
+
+    for city in cities:
+        res = requests.get(url.format(city)).json()
+        city_info={
+            'city':city.name,
+            'temp':res['main']['temp'],
+            'icon':res['weather'][0]['icon']
+        }
+        all_cities.append(city_info)
+
+
+    context={
+        'all_info':all_cities,
+        'form':form,
+    }
+
+    return render(request, 'weather/index.html',context)
 
 
 
